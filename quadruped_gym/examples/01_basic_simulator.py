bf63b2d536831b343a4f318969da1193363b67c2
Daniel Tan <dtch1997@users.noreply.github.com>
2022-11-03 16:34:26 +0000

    Simplify example 01

--- quadruped_gym/examples/01_basic_simulator.py
+++ quadruped_gym/examples/01_basic_simulator.py
@@ -1,65 +1,6 @@
 import numpy as np
-import gym
-import copy
-
-import typing
-from quadruped_gym.core.simulator import Controller, Simulator
-from quadruped_gym.core.types import RobotObservation, RobotAction
-
 from quadruped_gym.quadruped import a1_pybullet
-from quadruped_gym.quadruped.a1_pybullet.simulator import SimulationParameters  # noqa: F401
 from quadruped_gym.agents.linear_controller import LinearController
-
-class Logger:
-
-    def __init__(self):
-        self.data = []
-
-    def update(self, data: typing.Union[np.ndarray, RobotAction, RobotObservation]):
-        self.data.append(data)
-
-    def collate(self):
-        if isinstance(self.data[0], np.ndarray):
-            all_data = np.stack(self.data, axis=0)
-        elif isinstance(self.data[0], RobotAction):
-            all_data = RobotAction(
-                desired_motor_angles = np.stack([act.desired_motor_angles for act in self.data]),
-                desired_motor_velocities = np.stack([act.desired_motor_velocities for act in self.data]),
-                position_gain = np.stack([act.position_gain for act in self.data]),
-                velocity_gain = np.stack([act.velocity_gain for act in self.data]),
-                additional_torques = np.stack([act.additional_torques for act in self.data])
-            )
-        else:
-            all_data = RobotObservation(
-                base_position = np.stack([obs.base_position for obs in self.data]),
-                base_velocity = np.stack([obs.base_velocity for obs in self.data]),
-                base_orientation = np.stack([obs.base_orientation for obs in self.data]),
-                base_rpy = np.stack([obs.base_rpy for obs in self.data]),
-                base_rpy_rate = np.stack([obs.base_rpy_rate for obs in self.data]),
-                motor_angles = np.stack([obs.motor_angles for obs in self.data]),
-                motor_velocities = np.stack([obs.motor_velocities for obs in self.data]),
-                motor_torques = np.stack([obs.motor_torques for obs in self.data]),
-                foot_contacts = np.stack([obs.foot_contacts for obs in self.data]),
-                foot_contact_forces = np.stack([obs.foot_contact_forces for obs in self.data]),
-                foot_positions = np.stack([obs.foot_positions for obs in self.data]),
-            )
-        return all_data   
-
-def test_simulation_with_controller(simulator: Simulator, controller: Controller, n_time_steps = 100):
-    obs_logger = Logger()
-    act_logger = Logger()
-
-    obs = simulator.reset()
-    for i in range(n_time_steps):
-        controller.update(i * simulator.sim_params.n_action_repeat, obs)
-        action = controller.get_action()
-        obs_logger.update(copy.deepcopy(obs))
-        act_logger.update(copy.deepcopy(action))
-        obs = simulator.step(action)
-
-    observation_history = obs_logger.collate()
-    action_history = act_logger.collate()
-    return observation_history, action_history
 
 if __name__ == "__main__":
 
@@ -83,5 +24,8 @@ if __name__ == "__main__":
         interpolation_timesteps = 1000
     )
 
-    obs_hist, act_hist = test_simulation_with_controller(
-        simulator, controller, n_time_steps= N_TIME_STEPS)
+    obs = simulator.reset()
+    for i in range(N_TIME_STEPS):
+        controller.update(i * simulator.sim_params.n_action_repeat, obs)
+        action = controller.get_action()
+        obs = simulator.step(action)
