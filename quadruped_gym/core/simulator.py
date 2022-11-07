14a89cefe67e5be6b719acbb7a251e6d1242345e
Daniel Tan <dtch1997@users.noreply.github.com>
2022-11-07 15:31:24 +0000

    Remove dead code

--- quadruped_gym/core/simulator.py
+++ quadruped_gym/core/simulator.py
@@ -1,8 +1,7 @@
-from abc import ABC, abstractmethod
-
 import numpy as np
 
-from quadruped_gym.core.types import RobotAction, RobotObservation
+from abc import ABC, abstractmethod
+from quadruped_gym.core.types import RobotObservation
 
 
 class Simulator(ABC):
@@ -19,18 +18,4 @@ class Simulator(ABC):
     @abstractmethod
     def observe(self) -> RobotObservation:
         """Return the most recent robot observation"""
-        raise NotImplementedError
-
-
-class Controller(ABC):
-    def __init__(self):
-        self._current_time: int = None
-        self._robot_obs: RobotObservation = None
-
-    def update(self, current_time: int, robot_obs: RobotObservation):
-        self._current_time = current_time
-        self._robot_obs = robot_obs
-
-    @abstractmethod
-    def get_action(self) -> RobotAction:
         raise NotImplementedError
